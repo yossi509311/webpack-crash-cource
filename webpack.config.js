8fc7468f03363db401958b5830d2f68e71100212
yossi509311 <yossi509311@gmail.com>
2022-08-11 18:03:58 +0900

    implement uglifyjs-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 const outputPath = path.resolve(__dirname, 'dist')
 
@@ -52,5 +54,18 @@ module.exports  = {
         new MiniCssExtractPlugin({
             filename: '[name].[hash].css'
         })
-    ]
+    ],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    compress: {
+                        drop_console: true
+                    }
+                }
+            }),
+            // OptimizeCssAssetsPlugin
+            new OptimizeCssAssetsPlugin({})
+        ],
+    },
 }
